13cf114004f3c1a6a09c075e32bc16867c26afcd
HarleyY12 <hyang2496@gmail.com>
2025-06-08 20:31:41 -0400

    Created new class to calculate score based on length of connections.

---
+++ src/Connector.java
@@ -0,0 +1,11 @@
+public class Connector {
+    private GameBoard board;
+
+   public Connector (GameBoard board){
+       this.board = board;
+   }
+
+    public int calculateScore(){
+        return 0;
+    }
+}
