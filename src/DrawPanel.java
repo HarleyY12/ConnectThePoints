9e96bbdd989ed87a637f161ee78f0b6258ea9aeb
HarleyY12 <hyang2496@gmail.com>
2025-06-11 12:25:10 -0400

    Added mouse motion listener methods to reduce reliance on mouse clicked. Added code that allows the program to be playable by dragging.

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -2,23 +2,25 @@ import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class DrawPanel extends JPanel implements MouseListener{
+public class DrawPanel extends JPanel implements MouseListener, MouseMotionListener {
+    private Connector connector;
     private Rectangle resetButton;
     private Rectangle getNewPuzzle;
     private Rectangle pointsButton;
-    private boolean gameOver;
     private GameBoard board;
     private int cellSize;
     private int score;
-    private Connector connector;
-
-    private boolean hasStartedSelection;
     private int selectedValue;
+    private boolean hasStartedSelection;
+
     private List<Point> selectedPoints;
+    private List<Point> draggedPoints;
+
     private Color[] colorList = {
             Color.RED, Color.BLUE, Color.CYAN, Color.ORANGE, Color.PINK,
             Color.MAGENTA, Color.YELLOW, Color.GREEN };
@@ -33,18 +35,17 @@ public class DrawPanel extends JPanel implements MouseListener{
     public DrawPanel(GameBoard board){
         this.board = board;
         connector = new Connector(board);
-
         resetButton = new Rectangle(200,0,70,30);
         getNewPuzzle = new Rectangle(450,0,127,30);
         pointsButton = new Rectangle(300,0,120,30);
-        gameOver = false;
         addMouseListener(this);
+        addMouseMotionListener(this);
         cellSize = 60;
         score = 0;
-
-        hasStartedSelection = false;
         selectedValue = -1;
+        hasStartedSelection = false;
         selectedPoints = new ArrayList<>();
+        draggedPoints = new ArrayList<>();
     }
 
     protected void paintComponent(Graphics g){
@@ -52,8 +53,6 @@ public class DrawPanel extends JPanel implements MouseListener{
         int rows = board.getLength();
         int columns = board.getWidth();
         int [][] newBoard = board.getGameBoard();
-
-
 
         g.setFont(new Font("Courier New",Font.BOLD,20));
         for(int r = 0; r < rows; r++){
@@ -73,7 +72,6 @@ public class DrawPanel extends JPanel implements MouseListener{
                     g.fillRect(x,y,cellSize,cellSize);
                     g.setColor(Color.BLACK);
                     g.drawString(String.valueOf(value),x + 25,y + 35);
-
                 }
             }
         }
@@ -84,16 +82,11 @@ public class DrawPanel extends JPanel implements MouseListener{
         g.drawString("New Puzzle",455,23);
         g.drawRect(450,5,127,30);
         g.drawString("Score: " + score,305,23);
-
-
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         Point clicked = e.getPoint();
-        int row = (clicked.y - (int)(0.7 * cellSize))/cellSize;
-        int column = (clicked.x - (int)(0.5 * cellSize))/cellSize;
-        
         if(e.getButton() == 1){
             if(getNewPuzzle.contains(clicked)){
                 board.generateBoard();
@@ -111,57 +104,82 @@ public class DrawPanel extends JPanel implements MouseListener{
                 score = 0;
                 connector.resetConnection();
             }
-            if(column <= board.getWidth() && column >= 0 && row <= board.getLength() && row >=0){
-                int [][] game = board.getGameBoard();
-                int cell = game[row][column];
-                Point point = new Point(row,column);
+        }
+    }
 
-                if(!hasStartedSelection || cell > 0){
-                    if(cell > 0){
-                        if(hasStartedSelection && board.isOriginalPosition(row,column) &&
-                                selectedPoints.size() > 1){
-                            selectedPoints.add(point);
-                            int earnedPoints = connector.calculateScore(selectedValue,selectedPoints);
-                            if(earnedPoints > 0){
-                                score = score + earnedPoints;
-                                selectedValue = -1;
-                                selectedPoints = new ArrayList<>();
-                                hasStartedSelection = false;
-                                repaint();
-                            }else{
-                                selectedPoints.remove(selectedPoints.size()-1);
-                                repaint();
-                            }
-                        }
-                        hasStartedSelection = true;
-                        selectedValue = cell;
-                        selectedPoints = new ArrayList<>();
+    @Override
+    public void mousePressed(MouseEvent e) {
+        Point clicked = e.getPoint();
+        int row = (clicked.y - (int)(0.7 * cellSize))/cellSize;
+        int column = (clicked.x - (int)(0.5 * cellSize))/cellSize;
+        if(column <= board.getWidth() && column >= 0 && row <= board.getLength() && row >=0) {
+            int [][] game = board.getGameBoard();
+            int cell = game[row][column];
+            if(cell > 0 && board.isOriginalPosition(row,column)){
+                hasStartedSelection = true;
+                selectedValue = cell;
+                selectedPoints = new ArrayList<>();
+                draggedPoints = new ArrayList<>();
+                Point point = new Point(row,column);
+                selectedPoints.add(point);
+                draggedPoints.add(point);
+                repaint();
+            }
+        }
+    }
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if(hasStartedSelection){
+            Point clicked = e.getPoint();
+            int row = (clicked.y - (int)(0.7 * cellSize))/cellSize;
+            int column = (clicked.x - (int)(0.5 * cellSize))/cellSize;
+            if(column <= board.getWidth() && column >= 0 && row <= board.getLength() && row >=0) {
+                Point point = new Point(row,column);
+                if(!draggedPoints.contains(point)){
+                    int [][] game = board.getGameBoard();
+                    int cell = game[row][column];
+                    if(cell == 0 || cell == selectedValue && board.isOriginalPosition(row,column)) {
                         selectedPoints.add(point);
-                        repaint();
-                    }
-                }
-                else{
-                    if(cell == 0 && !selectedPoints.contains(point)){
-                        game[row][column] = selectedValue;
-                        selectedPoints.add(point);
+                        draggedPoints.add(point);
                         repaint();
                     }
                 }
             }
         }
     }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
     @Override
     public void mouseReleased(MouseEvent e) {
+        if(!hasStartedSelection || selectedPoints.size() < 2){
+            hasStartedSelection = false;
+            selectedValue = -1;
+            selectedPoints = new ArrayList<>();
+            draggedPoints = new ArrayList<>();
+        }
+        int earnedPoints = connector.calculateScore(selectedValue,selectedPoints);
+        if(earnedPoints > 0){
+            score = score + earnedPoints;
+            for(Point point : selectedPoints){
+                if(!board.isOriginalPosition(point.x,point.y)){
+                    board.getGameBoard()[point.x][point.y] = selectedValue;
+                }
+            }
+        }
+        hasStartedSelection = false;
+        selectedValue = -1;
+        selectedPoints = new ArrayList<>();
+        draggedPoints = new ArrayList<>();
+        repaint();
     }
+
+
     @Override
     public void mouseEntered(MouseEvent e) {
     }
     @Override
     public void mouseExited(MouseEvent e) {
+    }
+    @Override
+    public void mouseMoved(MouseEvent e) {
     }
 }
 
